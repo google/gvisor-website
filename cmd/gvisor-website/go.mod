af2a8b8fd5c77033ea01327432f0e6fd6b3a3398
Adin Scannell <ascannell@google.com>
2019-06-10 18:25:10 -0700

    Rewrite the synthetic Go branch as master.
    This enables legacy `go get` support, as well as vanilla `go get` with
    modules. In theory, one should now be able to run:
    
    ```
    go get gvisor.dev/gvisor/runsc
    go install gvisor.dev/gvisor/runsc
    ```
    
    And this should be always up-to-date.
    
    This support works by providing a synthetic git repository target at
    `/gvisor`. This repository passes through to the primary GitHub
    repository, but rewrites the *primary* branch to `refs/heads/go`. All
    other references are left exactly the same, but by default a checkout
    will use the Go branch.

--- cmd/gvisor-website/go.mod
+++ cmd/gvisor-website/go.mod
@@ -3,6 +3,55 @@ module main
 go 1.12
 
 require (
-	golang.org/x/oauth2 v0.0.0-20190226205417-e64efc72b421
+	github.com/BurntSushi/xgb v0.0.0-20160522181843-27f122750802 // indirect
+	github.com/aclements/go-gg v0.0.0-20170323211221-abd1f791f5ee // indirect
+	github.com/aclements/go-moremath v0.0.0-20190506201756-286cc0be6f75 // indirect
+	github.com/ajstarks/deck v0.0.0-20190526003814-edf08d731d5a // indirect
+	github.com/ajstarks/svgo v0.0.0-20181006003313-6ce6a3bcf6cd // indirect
+	github.com/bradfitz/gomemcache v0.0.0-20190329173943-551aad21a668 // indirect
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
+	github.com/coreos/pkg v0.0.0-20180928190104-399ea9e2e55f // indirect
+	github.com/cznic/cc v0.0.0-20181122101902-d673e9b70d4d // indirect
+	github.com/cznic/fileutil v0.0.0-20181122101858-4d67cfea8c87 // indirect
+	github.com/cznic/golex v0.0.0-20181122101858-9c343928389c // indirect
+	github.com/cznic/internal v0.0.0-20181122101858-3279554c546e // indirect
+	github.com/cznic/ir v0.0.0-20181122101859-da7ba2ecce8b // indirect
+	github.com/cznic/lex v0.0.0-20181122101858-ce0fb5e9bb1b // indirect
+	github.com/cznic/lexer v0.0.0-20181122101858-e884d4bd112e // indirect
+	github.com/cznic/mathutil v0.0.0-20181122101859-297441e03548 // indirect
+	github.com/cznic/strutil v0.0.0-20181122101858-275e90344537 // indirect
+	github.com/cznic/xc v0.0.0-20181122101856-45b06973881e // indirect
+	github.com/disintegration/gift v1.2.0 // indirect
+	github.com/edsrzf/mmap-go v1.0.0 // indirect
+	github.com/fogleman/gg v1.3.0 // indirect
+	github.com/garyburd/redigo v1.6.0 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/godbus/dbus v4.1.0+incompatible // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/gonum/blas v0.0.0-20181208220705-f22b278b28ac // indirect
+	github.com/gonum/floats v0.0.0-20181209220543-c233463c7e82 // indirect
+	github.com/gonum/internal v0.0.0-20181124074243-f884aa714029 // indirect
+	github.com/gonum/lapack v0.0.0-20181123203213-e4cdc5a0bff9 // indirect
+	github.com/gonum/matrix v0.0.0-20181209220409-c518dec07be9 // indirect
+	github.com/google/btree v1.0.0 // indirect
+	github.com/google/pprof v0.0.0-20190515194954-54271f7e092f // indirect
+	github.com/googleapis/gax-go v2.0.2+incompatible // indirect
+	github.com/ianlancetaylor/demangle v0.0.0-20181102032728-5e5cf60278f6 // indirect
+	github.com/jung-kurt/gofpdf v1.4.2 // indirect
+	github.com/kisielk/gotool v1.0.0 // indirect
+	github.com/mattn/go-sqlite3 v1.10.0 // indirect
+	github.com/nbutton23/zxcvbn-go v0.0.0-20180912185939-ae427f1e4c1d // indirect
+	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
+	github.com/phpdave11/gofpdi v1.0.3 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20190512091148-babf20351dd7 // indirect
+	github.com/rwcarlsen/goexif v0.0.0-20190401172101-9e8deecbddbd // indirect
+	github.com/stretchr/objx v0.2.0 // indirect
+	github.com/syndtr/goleveldb v1.0.0 // indirect
+	golang.org/x/build v0.0.0-20190610003023-a3d123a17ca9 // indirect
+	golang.org/x/image v0.0.0-20190523035834-f03afa92d3ff // indirect
+	golang.org/x/mobile v0.0.0-20190607214518-6fa95d984e88 // indirect
+	golang.org/x/oauth2 v0.0.0-20190402181905-9f3314589c9a
 	google.golang.org/api v0.4.0
+	gopkg.in/yaml.v1 v1.0.0-20140924161607-9f9df34309c0 // indirect
 )
--- cmd/gvisor-website/main.go
+++ cmd/gvisor-website/main.go
@@ -18,10 +18,15 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"os"
 	"regexp"
+	"sort"
+	"strconv"
 	"strings"
 
 	// For triggering manual rebuilds.
@@ -30,28 +35,37 @@ import (
 )
 
 var redirects = map[string]string{
-	"/change":    "https://gvisor.googlesource.com/gvisor/",
-	"/cl":        "https://gvisor-review.googlesource.com/",
+	"/change":    "https://github.com/google/gvisor",
 	"/issue":     "https://github.com/google/gvisor/issues",
 	"/issue/new": "https://github.com/google/gvisor/issues/new",
+	"/pr":        "https://github.com/google/gvisor/pulls",
 
 	// Redirects to compatibility docs.
 	"/c":             "/docs/user_guide/compatibility",
 	"/c/linux/amd64": "/docs/user_guide/compatibility/amd64",
+
+	// Deprecated, but links continue to work.
+	"/cl": "https://gvisor-review.googlesource.com",
 }
 
 var prefixHelpers = map[string]string{
-	"cl":     "https://gvisor-review.googlesource.com/c/gvisor/+/%s",
-	"change": "https://gvisor.googlesource.com/gvisor/+/%s",
+	"change": "https://github.com/google/gvisor/commit/%s",
 	"issue":  "https://github.com/google/gvisor/issues/%s",
+	"pull":   "https://github.com/google/gvisor/pull/%s",
 
 	// Redirects to compatibility docs.
 	"c/linux/amd64": "/docs/user_guide/compatibility/amd64/#%s",
+
+	// Redirect to the source viewer.
+	"gvisor": "https://github.com/google/gvisor/tree/go/%s",
+
+	// Deprecated, but links continue to work.
+	"cl": "https://gvisor-review.googlesource.com/c/gvisor/+/%s",
 }
 
 var (
 	validId     = regexp.MustCompile(`^[A-Za-z0-9-]*/?$`)
-	goGetHeader = `<meta name="go-import" content="gvisor.dev git https://github.com/google/gvisor">`
+	goGetHeader = `<meta name="go-import" content="gvisor.dev/gvisor git https://gvisor.dev/gvisor">`
 	goGetHTML5  = `<!doctype html><html><head><meta charset=utf-8>` + goGetHeader + `<title>Go-get</title></head><body></html>`
 )
 
@@ -194,6 +208,245 @@ func registerRebuild(mux *http.ServeMux) {
 	})))
 }
 
+// registerRepo registers the repository handler.
+func registerRepo(mux *http.ServeMux) {
+	if mux == nil {
+		mux = http.DefaultServeMux
+	}
+
+	mux.Handle("/gvisor/git-upload-pack", hostRedirectHandler(http.HandlerFunc(gitUploadPack)))
+	mux.Handle("/gvisor/info/refs", hostRedirectHandler(http.HandlerFunc(infoRefs)))
+}
+
+const (
+	upstreamInfoRefs      = "https://github.com/google/gvisor.git/info/refs?service=git-upload-pack"
+	upstreamGitUploadPack = "https://github.com/google/gvisor.git/git-upload-pack"
+)
+
+// targetURL is the URL object for upstreamGitUploadPack.
+var targetURL = func() *url.URL {
+	url, err := url.Parse(upstreamGitUploadPack)
+	if err != nil {
+		panic(err) // Should never happen.
+	}
+	return url
+}()
+
+// targetProxy is the reverse proxy for the main repository.
+var targetProxy = &httputil.ReverseProxy{
+	Director: func(req *http.Request) {
+		req.URL.Scheme = targetURL.Scheme
+		req.URL.Host = targetURL.Host
+		req.URL.Path = targetURL.Path
+		req.Header.Set("Host", targetURL.Host)
+	},
+}
+
+func gitUploadPack(w http.ResponseWriter, r *http.Request) {
+	// Proxy to the upstream repository.
+	targetProxy.ServeHTTP(w, r)
+}
+
+func infoRefs(w http.ResponseWriter, r *http.Request) {
+	// We intercept the client request. We implement only the appropriate
+	// git-upload-pack service and enforce that clients are asking for this
+	// as well. The full specification of this service, as well as a formal
+	// grammar is available at:
+	//
+	//	https://github.com/git/git/blob/master/Documentation/technical/http-protocol.txt
+	//
+	// We read only the first reference and change the primary branch. All
+	// other references and branches are left alone.
+
+	// Was this a dumb client? These are not supported.
+	vs, ok := r.URL.Query()["service"]
+	if !ok || len(r.URL.Query()) > 1 || len(vs) != 1 || vs[0] != "git-upload-pack" {
+		w.WriteHeader(http.StatusForbidden)
+		fmt.Fprintf(w, "invalid query: %v", r.URL.Query())
+		return
+	}
+
+	// Connect upstream.
+	client := new(http.Client)
+	resp, err := client.Get(upstreamInfoRefs)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "upstream get error: %v", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Read the full upstream contents.
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "upstream read error: %v", err)
+		return
+	}
+
+	// Was there an error? Pass all data through.
+	if resp.StatusCode != 200 {
+		w.WriteHeader(resp.StatusCode)
+		w.Write(data)
+		return
+	}
+
+	// emitPkt emits a single packet line.
+	emitPkt := func(m string) {
+		if len(m) == 0 {
+			// Special case: normally the size includes the size of
+			// the four byte header. However, we see that size 0000
+			// appears after the header and is the terminal. We use
+			// the empty string to indicate this.
+			fmt.Fprintf(w, "%04x", 0)
+		} else {
+			fmt.Fprintf(w, "%04x%s\n", 4+len(m)+1, m)
+		}
+	}
+
+	// readPkt reads a single packet line.
+	readPkt := func() (string, bool) {
+		// Parse the size header and return the string.
+		if len(data) < 4 {
+			return "", false
+		}
+		size, err := strconv.ParseInt(string(data[:4]), 16, 32)
+		if err != nil {
+			return "", false
+		}
+		if size == 0 {
+			data = data[4:]
+			return "", true
+		} else if len(data) >= int(size) {
+			m := string(data[4:size])
+			data = data[size:]
+			return strings.TrimSuffix(m, "\n"), true
+		} else {
+			return "", false
+		}
+	}
+
+	const (
+		// serviceLine is the first line emitted.
+		serviceLine = "# service=git-upload-pack"
+
+		// target is the target branch.
+		target = "refs/heads/go"
+	)
+
+	// Check the header.
+	header, ok := readPkt()
+	if !ok || header != serviceLine {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "invalid upstream header: %v", header)
+		return
+	}
+
+	// readRef reads a single reference.
+	readRef := func() (hash string, ref string, options []string, ok bool) {
+		line, ok := readPkt()
+		if line == "" && ok {
+			return "", "", nil, true
+		}
+		if !ok || line == "" {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "invalid reference: %v", line)
+			return "", "", nil, false
+		}
+		// Note that parts is guarnateed to be at least one element.
+		// Per the strings.Split documentation: "If s does not contain
+		// sep and sep is not empty, Split returns a slice of length 1
+		// whose only element is s."
+		parts := strings.Split(line, "\x00")
+		if len(parts) > 2 {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "invalid reference: %v", line)
+			return "", "", nil, false
+		}
+		if len(parts) == 2 {
+			options = strings.Split(parts[1], " ")
+		}
+		parts = strings.Split(parts[0], " ")
+		if len(parts) != 2 {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "invalid reference: %v", line)
+			return "", "", nil, false
+		}
+		return parts[0], parts[1], options, true
+	}
+
+	// Read any empty blocks. This does not appear to be generally part of
+	// the spec, but clients rely on at least a single blank message
+	// following the header. We just implement this faithfully.
+	headHash, first, options, ok := readRef()
+	for first == "" && ok {
+		headHash, first, options, ok = readRef()
+	}
+	if !ok {
+		return // Already sent error.
+	}
+
+	// Rewrite the options.
+	for i, option := range options {
+		if strings.HasPrefix(option, "symref=") {
+			// Rewrite to point to our branch target.
+			options[i] = fmt.Sprintf("symref=%s", target)
+		}
+	}
+
+	// Read all other references.
+	others := make(map[string]string)
+	order := make([]string, 0, 1)
+	for {
+		refHash, other, _, ok := readRef()
+		if !ok {
+			return // Already sent error.
+		}
+		if refHash == "" {
+			break // Terminal.
+		}
+		others[other] = refHash      // Save reference.
+		order = append(order, other) // Save order.
+	}
+
+	// Ensure our reference exists.
+	if _, ok := others[target]; !ok {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "invalid target reference: %v", target)
+		return
+	}
+
+	// If the original top-line reference was HEAD, then we need to rewrite
+	// this and the possible symref link. If it was not HEAD, then we need
+	// to actually change the ordering.
+	if first == "HEAD" {
+		headHash = others[target] // Just change the hash.
+	} else {
+		headHash = others[target]    // Rewrite the hash.
+		first = target               // And the reference.
+		others[first] = headHash     // Insert the regular reference.
+		order = append(order, first) // Insert the reference to the ordering.
+		sort.Strings(order)          // Sort resulting references.
+		delete(others, target)       // Drop the original reference.
+	}
+
+	// Required headers per the spec.
+	w.Header().Set("Content-Type", "application/x-git-upload-pack-advertisement")
+	w.Header().Set("Cache-Control", "no-cache")
+	emitPkt(serviceLine)
+	emitPkt("") // See above.
+	emitPkt(fmt.Sprintf("%s %s\x00%s", headHash, first, strings.Join(options, " ")))
+	for _, other := range order {
+		hash, ok := others[other]
+		if !ok {
+			// This should never happen if the above is correct.
+			panic(fmt.Sprintf("invalid other reference: %v", other))
+		}
+		emitPkt(fmt.Sprintf("%s %s", hash, other))
+	}
+	emitPkt("") // Terminal.
+}
+
 func envFlagString(name, def string) string {
 	if val := os.Getenv(name); val != "" {
 		return val
@@ -211,6 +464,7 @@ func main() {
 
 	registerRedirects(nil)
 	registerRebuild(nil)
+	registerRepo(nil)
 	registerStatic(nil, *staticDir)
 
 	log.Fatal(http.ListenAndServe(*addr, nil))
